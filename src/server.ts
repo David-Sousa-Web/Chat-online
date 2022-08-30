14fe29d281b07d8ec283ebb6e01200c58bb90237
David Sousa <david01sousa@Hotmail.com>
2022-08-29 23:22:20 -0300

    add post and get

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,22 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
-app.listen(3333, () => console.log("Server is running to on port 3333"))
+/**
+ * GET = buscas
+ * POST = Criação
+ * PUT = alteração
+ * DELETE = deletar
+ * PATCH = alterar uma informação especifica
+ */
+
+app.get("/", (request, response) =>{
+  return response.json({message: "Olá NLW 05"})
+
+})
+
+app.post("/", (request, response) =>{
+  return response.json({message: "Usuario salvo com sucesso!"})
+})
+
+app.listen(3333, () => console.log("Server is running on port 3333"))
